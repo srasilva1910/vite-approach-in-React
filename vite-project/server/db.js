d8e6303a9a7dcc78da7f6272ab1d3df38bf074c0
srasilva1910 <s.r.a.silva.1910@gmail.com>
2026-02-25 13:33:35 -0500

    initial commit

--- vite-project/server/db.js
+++ vite-project/server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:1nDcvLHCqkGFaXAmPT0UYUqy@172.21.248.51:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
